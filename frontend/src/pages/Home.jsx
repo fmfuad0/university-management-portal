12278619e498c33e4e92b21ee27f839ab7e82f39
fmfuad0 <fartinfuad@gmail.com>
2025-05-28 02:24:04 +0600

    ....

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,8 @@ import React from "react";
 const Home = () => {
   return (<>
 
-    <div style={{fontFamily: "Montserrat"}}
-         className="lg:tracking-widest sm:tracking-wide lg:text-3xl sm:text-xl pt-3 text-slate-900 lg:h-[80vh] bg-[url('./assets/GUB-cover.jpg')] lg:bg-[cover] sm:bg-[contain] sm:h-[60vh] justify-start font-bold items-center flex flex-col overflow-hidden">
+    <div style={{fontFamily: "Montserrat",backgroundImage: `url('./assets/GUB-cover.jpg')`} }
+         className={`lg:tracking-widest sm:tracking-wide lg:text-3xl sm:text-xl pt-3 text-slate-900 lg:h-[80vh] lg:bg-[cover] sm:bg-[contain] sm:h-[60vh] justify-start font-bold items-center flex flex-col overflow-hidden`}>
         <div className={`w-full h-full bg-black scale-110 opacity-60`}></div>
         <div className={`absolute mt-20 z-39 text-slate-300 flex flex-col items-center justify-start`}>
             <img src={`./assets/gumsbanner.png`} className={`w-[15%] mx-auto`} alt={``}/>
